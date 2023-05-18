8bf3a8d09956aba77be262d4dbc87fec57faf4a5
Carlos Aleman <caleman9791@protonmail.com>
2023-05-17 20:14:59 -0400

    ok
    ok

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -9,4 +9,18 @@ window.addEventListener("load", function(arguments) {
 	let main = document.getElementById("main");
 	main.setAttribute("style", "display: block !important");
 
+	function loadContent(url) {
+		fetch(url)
+			.then(response => response.text())
+			.then(data => {
+				document.getElementById('content').innerHTML = data;
+			})
+			.catch(error => {
+				console.error('Error al cargar el contenido:', error);
+			});
+	}
+	loadContent('./app/views/home/index.html');
+
 });
+
+// C:\wamp64\www\Proyectos\caleman9791\web\docs\app\views\home\index.html
