d271a4c146c8ac9814821c8639163439d41ee126
Carlos Aleman <caleman9791@protonmail.com>
2023-05-26 20:22:45 -0400

    ok

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,13 +1,13 @@
-'use strict'
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-requireDir('gulp-tasks');
+// 'use strict'
+// var gulp = require('gulp');
+// var requireDir = require('require-dir');
+// requireDir('gulp-tasks');
+// console.log(gulp);
 
+// gulp.paths = {
+//     dist: 'dist',
+// };
 
-gulp.paths = {
-    dist: 'dist',
-};
+// var paths = gulp.paths;
 
-var paths = gulp.paths;
-
-gulp.task('default', gulp.series('serve'));
+// gulp.task('default', gulp.series('serve'));
