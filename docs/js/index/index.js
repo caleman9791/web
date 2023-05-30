6eff9481f7496333111e1e7071bff4491539f2bc
Carlos Aleman <caleman9791@protonmail.com>
2023-05-30 10:10:23 -0400

    Tabulaciones

--- docs/js/index/index.js
+++ docs/js/index/index.js
@@ -1,11 +1,11 @@
-// enviacorreo(nombre, correo, mensaje);
-function inicio() {
-	// body...
-	let btn_inicio = document.querySelector(".btn.btn-warning.btn-fw.code_txt_btn_inicio");
-	btn_inicio.addEventListener("click", function () {
-		console.log(this);
-		window.location.href = "../../pag/login/login.html";
-		//  /pages/samples/login.html
-	});
-}
-inicio();
+// // enviacorreo(nombre, correo, mensaje);
+// function inicio() {
+// 	// body...
+// 	let btn_inicio = document.querySelector(".btn.btn-warning.btn-fw.code_txt_btn_inicio");
+// 	btn_inicio.addEventListener("click", function () {
+// 		console.log(this);
+// 		window.location.href = "../../pag/inicio/inicio.html";
+// 		//  /pages/samples/login.html
+// 	});
+// }
+// inicio();
