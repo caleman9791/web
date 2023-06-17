4768111af0dc84a272da544a48c3f4211be2e75e
Carlos Aleman <caleman9791@protonmail.com>
2023-06-17 03:11:43 -0400

    Librerías

--- docs/Gruntfile.js
+++ docs/Gruntfile.js
@@ -64,5 +64,5 @@ module.exports = function (grunt) {
     }
   });
   // the default task (running "grunt" in console) is "watch"
-  // grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['watch']);
 };
---
+++ docs/_caleman9791/app/js/index/app.js
@@ -0,0 +1,15 @@
+function index() {
+
+	let _img_vatar = document.querySelector('[caleman9791]');
+	console.log(_img_avtar);
+	// _img_vatar.setAttribute("src", "./_caleman9791/app/img/avatar.jpg");
+
+	// let xxxxx = document.querySelectorAll(".xxxxxxxxx");
+	// let xxxxx = document.querySelectorAll(".xxxxxxxxx");
+	// let xxxxx = document.querySelectorAll(".xxxxxxxxx");
+	// let xxxxx = document.querySelectorAll(".xxxxxxxxx");
+	// let xxxxx = document.querySelectorAll(".xxxxxxxxx");
+}
+window.onload = (event) => {
+	index();
+};
