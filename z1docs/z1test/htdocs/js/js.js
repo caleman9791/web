fa94f8923f1bb0f23f728cbd799d427163ffa467
Carlos Aleman <caleman9791@gmail.com>
2023-07-09 04:08:31 -0400

    PLntilla

--- z1docs/z1test/htdocs/js/js.js
+++
@@ -1,9 +0,0 @@
-// const request = window.indexedDB.open("MyTestDatabase", 3);
-// request.onerror = (event) => {
-// 	// Do something with request.errorCode!
-// 	console.log('Error');
-// };
-// request.onsuccess = (event) => {
-// 	// Do something with request.result!
-// 	console.log('OK');
-// };
